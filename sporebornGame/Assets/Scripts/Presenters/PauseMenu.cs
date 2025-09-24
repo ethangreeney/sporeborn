a2c4870f91d3513e6ec359b533146a78be4a30d1
katonCODE <danielhuang47@gmail.com>
2025-09-24 23:30:13 +1200

    In game pause menu completed, music added to actual game scene and volume slider logic hooked up with it. Added more robust pause menu handling

--- sporebornGame/Assets/Scripts/Presenters/PauseMenu.cs
+++ sporebornGame/Assets/Scripts/Presenters/PauseMenu.cs
@@ -1,27 +1,53 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems; 
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool isPaused = false;
     public GameObject pauseMenuUi;
+    public GameObject settingsMenuUI;
 
 
     // Update is called once per frame
     void Update()
     {
+        // Listen for the Escape key press
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (isPaused)
+            // CASE 1: The settings menu is currently open
+            if (settingsMenuUI.activeSelf)
             {
+                // Pressing Escape in settings should act like a "Back" button
+                CloseSettings();
+            }
+            // CASE 2: The game is already paused (and settings is not open)
+            else if (isPaused)
+            {
+                // Pressing Escape should resume the game
                 Resume();
             }
+            // CASE 3: The game is currently playing
             else
             {
+                // Pressing Escape should pause the game
                 Pause();
             }
         }
+    }
 
+    public void OpenSettings()
+    {
+        pauseMenuUi.SetActive(false);
+        settingsMenuUI.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenuUI.SetActive(false);
+        pauseMenuUi.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void Resume()
@@ -44,4 +70,6 @@ public class PauseMenu : MonoBehaviour
         SceneManager.LoadScene(0);
 
     }
+
+    
 }
--- sporebornGame/Assets/Scripts/Presenters/PauseMenuSettings.cs
+++ sporebornGame/Assets/Scripts/Presenters/PauseMenuSettings.cs
@@ -2,23 +2,36 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class PauseMenuSettings : MonoBehaviour
 {
 
-    public AudioMixer aMain;
-    public AudioMixer aSFX;
+    public AudioMixer mainMixer;
+    public Slider musicSlider;
+
+    private void OnEnable()
+    {
+        float musicVolumeDb;
+
+         if (mainMixer.GetFloat("MusicVolume", out musicVolumeDb))
+        {   
+            float linearVolume = Mathf.Pow(10, musicVolumeDb / 20);
+            musicSlider.value = linearVolume;
+        }
+         
+    }
 
     public void SetVolumeMain(float volume)
     {
-        aMain.SetFloat("MainVolume", volume);
+        mainMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
 
     }
 
 
     public void SetVolumeSFX(float volume)
     {
-        aSFX.SetFloat("MainVolume", volume);
+        mainMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
 
     }
 

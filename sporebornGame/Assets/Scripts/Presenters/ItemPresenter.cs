425acb3395a1fa591c1589eca38d7288cd2c0029
ethangreeneyy <ethan@greene.nz>
2025-10-13 21:17:54 +1300

    Code tweaks to ItemPresenter - Made ItemPresenter script use new itemPool plus Inventory.HasCollected system. - Simplified ItemPresenter code

--- sporebornGame/Assets/Scripts/Presenters/ItemPresenter.cs
+++ sporebornGame/Assets/Scripts/Presenters/ItemPresenter.cs
@@ -1,92 +1,35 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
+
 using UnityEngine;
 
 public class ItemPresenter : MonoBehaviour
 {
-    // Keeps track of item on this level
-    
-    private static GameObject ActiveItemInScene;
-    // All Items in Game
-    public List<GameObject> ItemList;
+    public ItemPool itemPool;
+    public PlayerInventory inventory;
+    private GameObject activeItem;
 
-    private HashSet<GameObject> spawnedItems = new HashSet<GameObject>();
-
-    System.Random rng;
-    void Start()
-    {
-        rng = new System.Random();
-        
-    }
-
-    // Called when the player enters the item room
-    // Add a parameter for the current room
     public void PlaceItemInItemRoom(Room room)
     {
-        // Don't spawn item if it's already been collected in this room
-        if (room.itemCollected)
-        {
-            return;
-        }
+        if (room.itemCollected) return;
+        if (room.assignedItemPrefab == null) room.assignedItemPrefab = PickRandomItem();
+        if (room.assignedItemPrefab == null) return;
 
-        // Assign item only once
-        if (room.assignedItemPrefab == null)
-        {
-            room.assignedItemPrefab = PickRandomItem();
-            if (room.assignedItemPrefab != null)
-            {
-                spawnedItems.Add(room.assignedItemPrefab);
-            }
-        }
-
-        if (room.assignedItemPrefab == null)
-        {
-            Debug.LogWarning("No item prefab assigned to room! Cannot spawn item. (Likely due to no more items in item pool");
-            return;
-        }
-
-        ActiveItemInScene = Instantiate(room.assignedItemPrefab, Vector3.zero, Quaternion.identity);
-
-        spawnedItems.Add(ActiveItemInScene);
-
-        // Set the room reference on the CollectionModel
-        var collectionModel = ActiveItemInScene.GetComponent<CollectionModel>();
-        if (collectionModel != null)
-        {
-            collectionModel.room = room;
-        }
+        activeItem = Instantiate(room.assignedItemPrefab, Vector3.zero, Quaternion.identity);
+        activeItem.GetComponent<CollectionModel>().room = room;
     }
 
-    // If player chooses not to collect item then 
-    // when they leave the room it shouldn't be there
     public void RemoveItemFromRoom()
     {
-        if (ActiveItemInScene != null)
-        {
-            Destroy(ActiveItemInScene);
-            spawnedItems.Remove(ActiveItemInScene);
-            ActiveItemInScene = null;
-        }
+        if (activeItem != null) Destroy(activeItem);
     }
 
-    // Gets random item GameObject
-    public GameObject PickRandomItem()
+    private GameObject PickRandomItem()
     {
-        var availableItems = ItemList.Where(item => !spawnedItems.Contains(item)).ToList();
-        if (availableItems.Count == 0)
-        {
-            // All items have been spawned, return null (at least for now)
-            return null;
-        }
-        return availableItems[rng.Next(0, availableItems.Count)];
+        return itemPool.itemPrefabs
+        .Where(Item => inventory.HasCollected(Item.GetComponent<CollectionModel>().item.itemName))
+        .OrderBy(_ => Random.value)
+        .FirstOrDefault();
     }
 
-    // Model notifys presenter
-    // Add a parameter for the current room
-    public void NotifyItemCollected(Room room)
-    {
-        room.itemCollected = true;
-        
-    }
+    public void NotifyItemCollected(Room room) => room.itemCollected = true;
 }

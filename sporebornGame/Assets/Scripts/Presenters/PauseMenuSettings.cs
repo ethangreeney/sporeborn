cf111fb2c9a1e7aaa413d950c1c419733a212a8d
katonCODE <danielhuang47@gmail.com>
2025-10-26 17:02:36 +1300

    SFX slider completed

--- sporebornGame/Assets/Scripts/Presenters/PauseMenuSettings.cs
+++ sporebornGame/Assets/Scripts/Presenters/PauseMenuSettings.cs
@@ -9,36 +9,39 @@ public class PauseMenuSettings : MonoBehaviour
 
     public AudioMixer mainMixer;
     public Slider musicSlider;
+    public Slider sfxSlider;
 
     private void OnEnable()
     {
         float musicVolumeDb;
 
-         if (mainMixer.GetFloat("MusicVolume", out musicVolumeDb))
+        if (mainMixer.GetFloat("MusicVolume", out musicVolumeDb))
         {   
             float linearVolume = Mathf.Pow(10, musicVolumeDb / 20);
             musicSlider.value = linearVolume;
         }
-         
+
+        float sfxVolumeDb;
+        if (mainMixer.GetFloat("SFXVolume", out sfxVolumeDb) && sfxSlider != null)
+        {
+            float linearSfx = Mathf.Pow(10f, sfxVolumeDb / 20f);
+            sfxSlider.value = linearSfx;
+        }
     }
 
     public void SetVolumeMain(float volume)
     {
         mainMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
-
     }
-
 
     public void SetVolumeSFX(float volume)
     {
-        mainMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
-
+        float db = (volume <= 0f) ? -80f : Mathf.Log10(volume) * 20f;
+        mainMixer.SetFloat("SFXVolume", db);
     }
-
 
     public void SetFullScreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
     }
-
 }

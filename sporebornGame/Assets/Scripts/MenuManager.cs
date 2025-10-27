d45eea1e882e12f5c8cb70871936598bf5dea253
ethangreeneyy <ethan@greene.nz>
2025-10-27 13:12:36 +1300

    made menu manager more concise (same functionality)

--- sporebornGame/Assets/Scripts/MenuManager.cs
+++ sporebornGame/Assets/Scripts/MenuManager.cs
@@ -1,15 +1,7 @@
 public static class MenuManager
 {
     private static bool isAnyMenuOpen = false;
-
-    public static bool TryOpenMenu()
-    {
-        return isAnyMenuOpen ? false : isAnyMenuOpen = true;
-    }
-
-    public static void CloseMenu()
-    {
-        isAnyMenuOpen = false;
-    }
+    public static bool TryOpenMenu() => isAnyMenuOpen ? false : isAnyMenuOpen = true;
+    public static void CloseMenu() => isAnyMenuOpen = false;
 }
 
